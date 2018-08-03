ab3c31fb30b8598414b3a9eaa4677704707d6826
tvitiello <thomas.vitiello.prestataire.sfr.com>
2018-08-03 17:08:29 +0200

    part2

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,9 +3,16 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use Symfony\Component\Routing\RequestContext;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use App\Repository\ArticleRepository;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 
 class BlogController extends Controller
 {
@@ -32,6 +39,39 @@ class BlogController extends Controller
     }
     
     /**
+     * @Route("/blog/new", name="blog_create")
+     */
+    public function create(Request $requete, ObjectManager $manager){
+        $article = new Article();
+        $form = $this->createFormBuilder($article)
+                     ->add('title', TextType::class, [
+                         'attr' => [
+                             'placeholder' => "Titre de l'article"
+                         ]
+                     ])
+                     ->add('content', TextareaType::class, [
+                         'attr' => [
+                             'placeholder' => "Contenu de l'article"
+                         ]
+                     ])
+                     ->add('image', TextType::class,[
+                         'attr' => [
+                             'placeholder' => "Image de l'article"
+                         ]
+                     ])
+                     ->add('save', SubmitType::class, [
+                         'label' => 'Ajouter l\'article'
+                     ])
+                     ->getForm();
+                     
+                     
+        
+        return $this->render('blog/create.html.twig', [
+            'formArticle' => $form->createView()
+        ]);
+    }
+    
+    /**
      * @Route("/blog/{id}", name="blog_show")
      */
     public function show(Article $article){
@@ -41,4 +81,5 @@ class BlogController extends Controller
             'article' => $article
         ]);
     }
+    
 }
